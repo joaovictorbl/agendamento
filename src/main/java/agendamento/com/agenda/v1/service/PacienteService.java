e9cdb1d4917290f8f25d4a09caad11a84c42cff6
joaovictorbl <joaovictor_bl@outlook.com>
2023-10-18 22:21:05 -0300

    ajuste metodo Ãe retornando a estudar java

--- src/main/java/agendamento/com/agenda/v1/service/PacienteService.java
+++ src/main/java/agendamento/com/agenda/v1/service/PacienteService.java
@@ -6,30 +6,43 @@ import agendamento.com.agenda.v1.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional /* Anotação que impede quando algum de errado não concletiza a TRANSAÇÃO*/
 public class PacienteService {
+
+    //TODO: inserir logs
+    //TODO: melhorar metodo de validação de email é cpf
+    //TODO: melhorar exeçoes com hedler
 
     @Autowired
     private PacienteRepository repository;
 
     public Paciente salvar(Paciente paciente) {
 
-        Boolean existeCPF = false;
-        Optional<Paciente> base = repository.findByCpf(paciente.getCpf());
+        Boolean variavelDeControle = false;
+        Optional<Paciente> baseDeDados = repository.findByCpf(paciente.getCpf());
 
-        if (base.isPresent()){
-            if (!base.get().getId().equals(paciente.getId())){
-                existeCPF = true;
+        if (baseDeDados.isPresent()){
+            if (!baseDeDados.get().getId().equals(paciente.getId())){
+                variavelDeControle = true;
             }
         }
 
-        if (existeCPF){
-            throw new businessExcepetion("Paciente já cadastrado!");
+        baseDeDados = repository.findByEmail(paciente.getEmail());
+
+        if (baseDeDados.isPresent()){
+            if (!baseDeDados.get().getId().equals(paciente.getId())){
+                variavelDeControle = true;
+            }
         }
 
+        if (variavelDeControle){
+            throw new businessExcepetion("Paciente já cadastrado!");
+        }
 
         return repository.save(paciente);
     }
